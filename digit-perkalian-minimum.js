93909c76ad7bccb4f1aab53d7750200d48fe897b
Agustinus Adam <agustinusadamw@gmail.com>
2020-02-12 12:13:36 +0700

    Update code dari file digit-perkalian-minimum.js

--- digit-perkalian-minimum.js
+++ digit-perkalian-minimum.js
@@ -20,13 +20,33 @@ function digitPerkalianMinimum(angka) {
         }
     }
 
-    var minimal = temp2[temp2.length - 1].length
+    var temp3 = []
+    for (k = 0; k < temp2.length; k++) {
+        temp3.push(Number(temp2[k]))
+    }
+
+    // Sorting dari yang terbesar ke terkecil
+    for (l = 0; l < temp3.length; l++) {
+        for (m = 0; m < temp3.length; m++) {
+            if (temp3[m] < temp3[m + 1]) {
+                var sementara = temp3[m + 1]
+                temp3[m + 1] = temp3[m]
+                temp3[m] = sementara
+            }
+        }
+    }
+
+    var temp4 = []
+    for (o = 0; o < temp3.length; o++) {
+        temp4.push(String(temp3[o]))
+    }
+
+    var minimal = temp4[temp4.length - 1].length
     return minimal
 }
-  
-// TEST CASES
+
 console.log(digitPerkalianMinimum(24)); // 2
 console.log(digitPerkalianMinimum(90)); // 3
 console.log(digitPerkalianMinimum(20)); // 2
 console.log(digitPerkalianMinimum(179)); // 4
-console.log(digitPerkalianMinimum(1)); // 2 
+console.log(digitPerkalianMinimum(1)); // 2
